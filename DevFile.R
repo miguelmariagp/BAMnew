16b58dd6bc4b057f1f3dd26150d3a6b986c18b5d
miguelmariagp <m.pereira@wustl.edu>
2016-03-31 17:56:18 -0500

    Starting package

---
+++ DevFile.R
@@ -0,0 +1,18 @@
+#Development File
+#BaM
+library(devtools)
+library(roxygen2)
+
+#Set your working directory
+#Miguel
+setwd("C:/Users/ststest/Dropbox/Spr16/Programming/BAMnew") #This will need to be changed to match your directory
+
+#create("BaM2")
+
+current.code <- as.package("BaM2")
+load_all(current.code)
+document(current.code)
+
+#You can check the 'old' version of the package here:
+library(BaM)
+?biv.norm.post
