93904486bdcb45ade2149bf2d075a1dfaa4d50f4
Danielle Korman <d.korman@wustl.edu>
2016-04-05 13:40:38 -0500

    First 3 data sets

---
+++ BaM2/R/Actuarial.R
@@ -0,0 +1,14 @@
+#' Actuarial
+#'
+#' 
+#'
+#' The variables included in the dataset are:
+#' \itemize{
+#' \item\code{year} 
+#' \item\code{payroll} 
+#' \item\code{claims} 
+#'}
+#'
+#' @name actuarial
+#' @docType data
+NULL
---
+++ BaM2/R/africa.R
@@ -0,0 +1,21 @@
+#' africa
+#'
+#' African Coups Data, pp.551-552
+#'
+#' The variables included in the dataset are:
+#' \itemize{
+#' \item\code{MILTCOUP} 
+#' \item\code{MILITARY} 
+#' \item\code{POLLIB}
+#' \item\code{PARTY93}
+#' \item\code{PCTVOTE}
+#' \item\code{PCTTURN}
+#' \item\code{SIZE}
+#' \item\code{POP}
+#' \item\code{NUMREGIM}
+#' \item\code{NUMELEC}
+#'} 
+#'
+#' @name africa
+#' @docType data
+NULL
