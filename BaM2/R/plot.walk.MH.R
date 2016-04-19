01f2b95cfb2e25e391159ef2fd9a0ea25614b03a
Mauricio Vela <mauricioandresvela@gmail.com>
2016-04-19 00:12:42 -0400

    changing plot.walk

--- BaM2/R/plot.walk.MH.R
+++ BaM2/R/plot.walk.MH.R
@@ -12,7 +12,7 @@
 mhplot <- function(walk.mat)
 UseMethod("mhplot")
 mhplot.walk.MH <- function(walk.mat)  {
-    mhplot(walk.mat[1,1],walk.mat[1,2],type="n",
+    plot(walk.mat[1,1],walk.mat[1,2],type="n",
         xlim=round(range(walk.mat[,1])*1.2),
         ylim=round(range(walk.mat[,2])*1.2),
 	xlab="",ylab="")
