9b74632b03b4fcc0f9c7d69f18a694ffb545e5b1
Mauricio Vela <mauricioandresvela@gmail.com>
2016-04-21 15:07:31 -0400

    italian

--- BaM2/R/italy.parties-data.R
+++ BaM2/R/italy.parties-data.R
@@ -1,6 +1,6 @@
 #' @name italy.parties
 #' @title italy.parties
-#' @description Italian Parties Data. See page 389
+#' @description Italian Parties Data. See page 370
 #' @usage data(italy.parties)
 #' @docType data
 NULL
