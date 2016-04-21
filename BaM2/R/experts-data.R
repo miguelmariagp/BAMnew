26e914928b10fd50b774abe9e3f7cda14f8f9be0
Mauricio Vela <mauricioandresvela@gmail.com>
2016-04-21 14:55:58 -0400

    experts data

--- BaM2/R/experts-data.R
+++ BaM2/R/experts-data.R
@@ -1,6 +1,6 @@
 #' @name experts
 #' @title Campaign fundraisign elicitations
-#' @description Fabricated data on campaign fundraising elicitations. See page 164
+#' @description Fabricated data on campaign fundraising elicitations. See page 120
 #' @usage experts(q1,q2,q3)
 #' @param q1 the 0.1 quantile
 #' @param q2 the 0.5 quantile
