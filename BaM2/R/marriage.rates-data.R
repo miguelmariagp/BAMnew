57c238ba9b9e0bf4bedea2f979e65da247474915
jmetz2131 <jacob.metz@wustl.edu>
2016-04-26 14:12:23 -0500

    Added Variables to help file and added dataset
    I changed the marriage.rates dataset to be a vector as it appears in
    the book and I updated both the marriage.rates and wars datasets.

--- BaM2/R/marriage.rates-data.R
+++ BaM2/R/marriage.rates-data.R
@@ -2,7 +2,7 @@
 #' @title Marriage Rates in Italy
 #' @description Italian Marriage Rates. See page 430
 #' @usage data(marriage.rates)
-#' @format a vector containing 15 numbers
+#' @format a vector containing 16 numbers
 #' @source Columbo, B. (1952). Preliminary Analysis of Recent Demographic Trends in Italy. Population Index 18, 265-279.
 #' @docType data
 NULL
