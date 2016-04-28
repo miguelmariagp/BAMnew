f3d94a090e37d9ecde8da45327ae1d221b814cb0
Danielle Korman <d.korman@wustl.edu>
2016-04-28 13:55:44 -0500

    added executions variables

--- BaM2/R/executions-data.R
+++ BaM2/R/executions-data.R
@@ -1,6 +1,18 @@
 #' @name executions
 #' @title executions
+#' 
+#' #' The variables included in the dataset are:
+#' \itemize{
+#' \item\code{State} State
+#' \item\code{EXECUTIONS} Number of capital punishments at state level in 1997
+#' \item\code{Median.Income} Median per capita income in dollars
+#' \item\code{Percent.Poverty} Percent classified as living in poverty
+#' \item\code{Percent.Black} Percent of black citizens in population
+#' \item\code{Violent.Crime} Rate of violent crime per 100,000 residents for 1996
+#'}
+#'
 #' @description Execution data.
+#' @format explanatory variables for 17 states
 #' @docType data
 #' @usage data(executions)
 NULL
